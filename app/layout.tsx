3858eb6b2df68338f001825febfb2e1684a6a006
Nylio-prog <nils.cercariolo@gmail.com>
2023-06-25 23:14:47 +0900

    added toast and log out

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Figtree } from 'next/font/google'
 import SupabaseProvider from '@/providers/SupabaseProvider'
 import UserProvider from '@/providers/UserProvider'
 import ModalProvider from '@/providers/ModalProvider'
+import ToasterProvider from '@/providers/ToasterProvider'
 
 const figtree = Figtree({ subsets: ['latin'] })
 
@@ -20,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={figtree.className}>
+        <ToasterProvider />
         <SupabaseProvider>
           <UserProvider>
             <ModalProvider />
